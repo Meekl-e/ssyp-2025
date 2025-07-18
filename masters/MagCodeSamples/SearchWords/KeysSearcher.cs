b31d0cece8503777878781b7f1fdf58798f03477
Meekl-e <108610643+Meekl-e@users.noreply.github.com>
2025-07-18 11:29:31 +0700

    Add SearchWords

--- masters/MagCodeSamples/SearchWords/KeysSearcher.cs
+++ masters/MagCodeSamples/SearchWords/KeysSearcher.cs
@@ -6,22 +6,64 @@ using System.Threading.Tasks;
 
 namespace SearchWords
 {
-    public class DataSource<T>
+    /// <summary>
+    /// Интерфейс источника данных
+    /// </summary>
+    /// <typeparam name="D">тип элемента данных (документа)</typeparam>
+    /// <typeparam name="K">ключ по которому дается доступ к документу</typeparam>
+    public interface IDataSource<D, K>
+    {
+        // Обычный конструктор имеет параметром поток элементов (документов)
+        IEnumerable<D> Elements();
+        IEnumerable<Tuple<D, K>> ElementsDK(); // Документ и его ключ
+        D GetElement(K key);
+        IEnumerable<K> WordToKeys(string word);
+        Func<D, IEnumerable<string>> GetDWFunc();
+    }
+    public class WordsSearcher<D, K>
+    {
+        private IDataSource<D, K> dsource;
+        private Func<D, IEnumerable<string>> dwFunc;
+        private Dictionary<string, K[]> wordToKeys;
+        public WordsSearcher(IDataSource<D, K> dsource)
+        {
+            this.dsource = dsource;
+            this.dwFunc = dsource.GetDWFunc();
+
+            wordToKeys = dsource.ElementsDK()
+                .SelectMany(dk => dwFunc(dk.Item1)
+                    .Select(w => { var k = dk.Item2; return (w, k); }))
+                .GroupBy(wk => wk.w, wk => wk.k)
+                .ToDictionary(igr => igr.Key, igr => igr.ToArray());
+        }
+        public void Search(string[] words)
+        {
+            var query = words.SelectMany(w => { if (wordToKeys.TryGetValue(w, out K[] karr)) return karr; else return new K[0]; })
+                .GroupBy(k => k)
+                .Select(igr => (igr.Key, igr.Count()))
+                .OrderByDescending(k => k.Item2)
+                .ToArray()
+                ;
+            foreach (var pair in query)
+            {
+                Console.WriteLine($"group: {dsource.GetElement(pair.Key)} {pair.Item2}");
+                //foreach (int i in group)
+                //{
+                //    Console.WriteLine(i);
+                //}
+            }
+        }
+    }
+
+    /// <summary>
+    /// Источник данных. Сохраняет какое-то множество данных типа T с использованием ключа типа K
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <typeparam name="K"></typeparam>
+    public class DataSource<T, K>
     {
         private T[] docs;
         public DataSource(IEnumerable<T> documents) { }
     }
-    public class KeysSearcher
-    {
-        private object[] docs;
-        private Dictionary<string, int[]> wordToIndexes;
-        public KeysSearcher(object[] docs, Func<object, IEnumerable<string>> ToWordsFunc) 
-        { 
-            this.docs = docs;
-            wordToIndexes = docs.SelectMany((d, i) => ToWordsFunc(d).Select(w => (w, i)))
-                .GroupBy(wi => wi.w, wi => wi.i)
-                .ToDictionary(igr => igr.Key, igr => igr.ToArray());
-        }
-        public object Search
-    }
+
 }
--- masters/MagCodeSamples/SearchWords/Program.cs
+++ masters/MagCodeSamples/SearchWords/Program.cs
@@ -1,8 +1,66 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
-using SearchWords;
+namespace SearchWords
+{
+    class Program
+    {
+        public static void Main()
+        {
+            List<int> numbers = new List<int>(new int[] { 35, 44, 200, 84, 3987, 4, 199, 329, 446, 208 });
 
-Console.WriteLine("Start SearchWords");
+            IEnumerable<IGrouping<int, int>> query = numbers
+                .GroupBy(number => number % 2);
 
-string[] docs = new string[] { "qwe rty uiop", "asd fgh jkl", "qwe aaa" };
-KeysSearcher searcher = new KeysSearcher(docs.Select(s => (object)s).ToArray(), ob => ((string)ob).Split(' '));
+            foreach (var group in query)
+            {
+                Console.WriteLine(group.Key == 0 ? "\nEven numbers:" : "\nOdd numbers:");
+                foreach (int i in group)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.WriteLine("Start SearchWords");
+
+            string[] docs = new string[] { "qwe rty", "asd fgh jkl", "qwe aaa uiop" };
+            
+            DataSourceList dsl = new DataSourceList(docs);
+            WordsSearcher<string, int> searcher = new WordsSearcher<string, int>(dsl);
+            searcher.Search("sdl uiop qwe".Split(' '));
+        }
+    }
+    class DataSourceList : IDataSource<string, int>
+    {
+        private string[] list;
+        public DataSourceList(IEnumerable<string> flow)
+        {
+            list = flow.ToArray();
+        }
+        public IEnumerable<string> Elements()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Tuple<string, int>> ElementsDK() // Выдает поток документов с их ключами
+        {
+            return list.Select((d, i) => new Tuple<string, int>(d, i));
+        }
+
+        public Func<string, IEnumerable<string>> GetDWFunc() // преобразователь документа в поток строк
+        {
+            return (string line) => line.Split(' ').Distinct();
+        }
+
+        public string GetElement(int key)
+        {
+            return list[key];
+        }
+
+        public IEnumerable<int> WordToKeys(string word)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
