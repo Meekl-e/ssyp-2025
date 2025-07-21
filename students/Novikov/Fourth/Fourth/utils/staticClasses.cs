4dd57a539e6194e5a64f7e0322bdc74b81af7055
Участник олимпиады <programmer@main.nsu.ru>
2025-07-21 13:20:09 +0700

    separating htmls

--- students/Novikov/Fourth/Fourth/utils/staticClasses.cs
+++ students/Novikov/Fourth/Fourth/utils/staticClasses.cs
@@ -3,42 +3,28 @@ public static class HtmlPage
 {
     public static string GetHtml(string title, string snippet)
     {
-        return @$"
-               <!DOCTYPE html>
-                <html lang = 'ru'>
-                 <head>
-                     <meta charset = 'UTF-8'>
-                      <title>{title}</title>
-                     </head>
-                     <body>
-                     <a href='/'>На главную</a>
-                     <br>
-               {snippet}
-               </body>
-               </html>
-               ";
+        return File.ReadAllText(Directory.GetCurrentDirectory() + @"\wwwroot\htmls\2.html")
+        .Replace("{{ snippet }}", snippet)
+        .Replace("{{ title }}", title);
     }
 
     public static string GetHtml1(string title, string snippet)
         {
-            return @$"
-                   <!DOCTYPE html>
-                    <html lang = 'ru'>
-                     <head>
-                         <meta charset = 'UTF-8'>
-                          <title>{title}</title>
-                         </head>
-                         <body>
-                   {snippet}
-                   </body>
-                   </html>
-                   ";
+        return File.ReadAllText(Directory.GetCurrentDirectory() + @"\wwwroot\htmls\3.html")
+        .Replace("{{ snippet }}", snippet)
+        .Replace("{{ title }}", title);
         }
 
     public static string GetMainHtml(int vk, int tg, int oB, int cNews, int academC, int elementy)
     {
 
-        return File.ReadAllText(Directory.GetCurrentDirectory()+ @"\wwwroot\htmls\index.html").Replace("{{ vk }}", vk.ToString()).Replace("{{ tg }}", tg.ToString()).Replace("{{ oB }}", oB.ToString()).Replace("{{ cNews }}", cNews.ToString()).Replace("{{ academC }}", academC.ToString()).Replace("{{ elementy }}", elementy.ToString());
+        return File.ReadAllText(Directory.GetCurrentDirectory() + @"\wwwroot\htmls\index.html")
+        .Replace("{{ vk }}", vk.ToString())
+        .Replace("{{ tg }}", tg.ToString())
+        .Replace("{{ oB }}", oB.ToString())
+        .Replace("{{ cNews }}", cNews.ToString())
+        .Replace("{{ academC }}", academC.ToString())
+        .Replace("{{ elementy }}", elementy.ToString());
     }
 }
 
