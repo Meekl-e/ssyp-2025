4256ae8c62d9438bb3116eab9f9af8b8d1393e0b
Meekl-e <108610643+Meekl-e@users.noreply.github.com>
2025-07-23 16:56:35 +0700

    Delete students directory

--- students/Novikov/Fourth/Fourth/dataBaseControllers/classes/MorphStaticClass.cs
+++
@@ -1,7 +0,0 @@
-
-using Nestor;
-
-public static class Morph
-{
-    public static NestorMorph nestorMorph = new NestorMorph();
-}
--- students/Novikov/Fourth/Fourth/wwwroot/javascripts/script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-
-    document.getElementById("blurred_el").classList.add("loaded");
-    document.getElementById("blurred_el1").classList.add("loaded");
-    document.getElementById("blurred_el2").classList.add("loaded");
-    document.getElementById("blurred_el3").classList.add("loaded");
-});
