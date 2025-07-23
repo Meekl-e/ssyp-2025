c63fffbf213883fa502e8f516c73f942b558b8dc
Участник олимпиады <programmer@main.nsu.ru>
2025-07-23 16:16:02 +0700

    site almost done upd

--- students/Novikov/Fourth/Fourth/wwwroot/javascripts/script.js
+++ students/Novikov/Fourth/Fourth/wwwroot/javascripts/script.js
@@ -1,25 +1,3 @@
-document.addEventListener("DOMContentLoaded", function () {
-    let button = document.getElementById("theme");
-    let li = document.getElementsByTagName('li');
-    let dark_theme = true;
-    let button2 = document.getElementById('cs2');
-    button.addEventListener("click", function() {
-
-        if(dark_theme === true) {
-            document.body.style.color = 'white'
-            document.body.style.backgroundColor = 'black'
-            dark_theme = false;
-        } else {
-            dark_theme = true;
-            document.body.style.color = 'black'
-            document.body.style.backgroundColor = '#ffffd8'
-
-        }
-    });
-
-
-});
-
 document.addEventListener("DOMContentLoaded", function() {
 
     document.getElementById("blurred_el").classList.add("loaded");
