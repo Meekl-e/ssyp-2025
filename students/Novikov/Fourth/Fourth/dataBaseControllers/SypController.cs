82acc469547e8d79495c4d888c8b8f48d1e643f0
Участник олимпиады <programmer@main.nsu.ru>
2025-07-23 10:24:06 +0700

    fix problems with syp

--- students/Novikov/Fourth/Fourth/dataBaseControllers/SypController.cs
+++ students/Novikov/Fourth/Fourth/dataBaseControllers/SypController.cs
@@ -70,19 +70,24 @@ public class Controller
         xDB.Elements().Single(x => x.Attribute("prop").Value == "in-org")
             .Elements().Select(person =>
         {
-            string personId = person.Elements().Single(x => x.Attribute("prop").Value == "participant")
-            .Elements().Single(x => x.Attribute("tp").Value == "person")
-                .Attribute("id")
-                    .Value;
-
-            string personName = person.Elements().Single(x => x.Attribute("prop").Value == "participant")
+            if (person.Elements().Single(x => x.Attribute("prop").Value == "participant")
+                    .Elements().Where(x => x.Attribute("tp").Value == "person").Any())
+            {
+                string personId = person.Elements().Single(x => x.Attribute("prop").Value == "participant")
                 .Elements().Single(x => x.Attribute("tp").Value == "person")
-                .Elements().Single(x => x.Attribute("prop").Value == "name")
-                .Elements().Single(x => x.Name.LocalName == "v")
-                    .Value;
-            return new XElement("div",
-            new XElement("a", new XAttribute("href", $"person?id={personId}"), $"{personName}"),
-            new XElement("br"));
+                    .Attribute("id")
+                        .Value;
+
+                string personName = person.Elements().Single(x => x.Attribute("prop").Value == "participant")
+                    .Elements().Single(x => x.Attribute("tp").Value == "person")
+                    .Elements().Single(x => x.Attribute("prop").Value == "name")
+                    .Elements().Single(x => x.Name.LocalName == "v")
+                        .Value;
+                return new XElement("div",
+                new XElement("a", new XAttribute("href", $"person?id={personId}"), $"{personName}"),
+                new XElement("br"));
+            }
+            return null;
         }),
         xDB.Elements().Select(imgs =>
         {
@@ -92,32 +97,17 @@ public class Controller
             }
             return new XElement("div", imgs.Elements().Select(photo =>
             {
-                string uri = photo.Elements().Single(x => x.Attribute("prop").Value == "in-doc")
-                        .Elements().Single(x => x.Attribute("tp").Value == "photo-doc")
-                        .Elements().Single(x => x.Attribute("prop").Value == "uri").Value;
-                return new XElement("img", new XAttribute("src", $"http://syp.iis.nsk.su/SypWebApi/photo?uri={uri}&s=medium"));
+                if (photo.Elements().Single(x => x.Attribute("prop").Value == "in-doc")
+                            .Elements().Where(x => x.Attribute("tp").Value == "photo-doc").Any())
+                {
+                    string uri = photo.Elements().Single(x => x.Attribute("prop").Value == "in-doc")
+                            .Elements().Single(x => x.Attribute("tp").Value == "photo-doc")
+                            .Elements().Single(x => x.Attribute("prop").Value == "uri").Value;
+                    return new XElement("img", new XAttribute("src", $"http://syp.iis.nsk.su/SypWebApi/photo?uri={uri}&s=medium"));
+                }
+                return null;
             }));
-        })
-        // ,
-        // xDB.Elements().Select(studios =>
-        // {
-        //     if (studios.Attribute("prop").Value == "org-parent")
-        //     {
-        //         return null;
-        //     }
-        //     return new XElement("div", studios.Elements().Select(studio =>
-        //     {
-        //         string studioId = studio.Elements().Single(x => x.Attribute("prop").Value == "org-child")
-        //                 .Elements().Single(x => x.Attribute("tp").Value == "org-sys")
-        //                 .Attribute("id").Value;
-        //         return new XElement("a", new XAttribute("href", $"http://syp.iis.nsk.su/SypWebApi/xml/tree/{id}"),
-        //         studio.Elements().Single(x => x.Attribute("prop").Value == "org-child")
-        //                 .Elements().Single(x => x.Attribute("tp").Value == "org-sys")
-        //                 .Elements().Single(x => x.Attribute("prop").Value == "name")
-        //                 .Elements().Single(x => x.Name.LocalName == "v").Value);
-        //     }));
-        // }
-        );
+        }));
 
         return HtmlPage.GetField("", html.ToString());
     }
