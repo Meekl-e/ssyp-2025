aab31fa8ec645717b6f188d68aa1a1235059877d
Участник олимпиады <programmer@main.nsu.ru>
2025-07-23 10:26:00 +0700

    modified:   .gitignore modified:   students/Novikov/Fourth/Fourth/wwwroot/css/index.css 	new file:   students/Novikov/Fourth/Fourth/wwwroot/datasets/image/korob.png 	modified:   students/Novikov/Fourth/Fourth/wwwroot/htmls/index.html 	modified:   students/Novikov/Fourth/Fourth/wwwroot/javascripts/script.js

--- students/Novikov/Fourth/Fourth/wwwroot/javascripts/script.js
+++ students/Novikov/Fourth/Fourth/wwwroot/javascripts/script.js
@@ -20,3 +20,10 @@ document.addEventListener("DOMContentLoaded", function () {
 
 });
 
+document.addEventListener("DOMContentLoaded", function() {
+
+    document.getElementById("blurred_el").classList.add("loaded");
+    document.getElementById("blurred_el1").classList.add("loaded");
+    document.getElementById("blurred_el2").classList.add("loaded");
+    document.getElementById("blurred_el3").classList.add("loaded");
+});
